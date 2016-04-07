e9edd493a226dacd9bbe1c8bc1c125b24801708e
Andrey Bogoyavlensky <abogoyavlensky@gmail.com>
2016-04-07 15:26:28 +0300

    Add decorator for coroutine.

--- python_essential_reference/page143.py
+++ python_essential_reference/page143.py
@@ -1,11 +1,21 @@
-def coro():
+def coro(func):
+
+    def start(*args, **kwargs):
+        f = func(*args, **kwargs)
+        f.next()
+        return f
+
+    return start
+
+
+@coro
+def receiver():
     while True:
         n = (yield)
         print 'Recieved: {}'.format(n)
 
 
 if __name__ == '__main__':
-    c = coro()
-    c.next()
-    c.send(10)
-    c.send('Hello!')
+    r = receiver()
+    r.send(10)
+    r.send('Hello!')
