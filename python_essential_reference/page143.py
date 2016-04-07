e7d514caf287d6e820bcf02aa0644aed9b5895e3
Andrey Bogoyavlensky <abogoyavlensky@gmail.com>
2016-04-07 16:03:14 +0300

    Add ability to return result from coroutine.

--- python_essential_reference/page143.py
+++ python_essential_reference/page143.py
@@ -15,7 +15,19 @@ def receiver():
         print 'Recieved: {}'.format(n)
 
 
+@coro
+def linebreaker(delimiter=' '):
+    result = None
+    while True:
+        line = (yield result)
+        result = line.split(delimiter)
+
+
 if __name__ == '__main__':
     r = receiver()
     r.send(10)
     r.send('Hello!')
+
+    b = linebreaker(',')
+    print(b.send('a,b,c,d'))
+    print(b.send('1,2,3,4'))
