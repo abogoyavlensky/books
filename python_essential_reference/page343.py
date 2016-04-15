6c04a8074d19ab970462b82add42f3e7bf3b722e
Andrey Bogoyavlensky <abogoyavlensky@gmail.com>
2016-04-15 09:57:48 +0300

    Add example with count from itertools.

--- python_essential_reference/page343.py
+++ python_essential_reference/page343.py
@@ -7,9 +7,14 @@
 
     :copyright: (c) 2016 by Rambler&Co.
 """
-from itertools import combinations
-
+from itertools import combinations, count
 
 if __name__ == '__main__':
     result = combinations([1, 2, 3, 4], 2)
     print(list(result))
+
+    c = count(0)
+    i = c.next()
+    while i < 10:
+        print(i)
+        i = c.next()
