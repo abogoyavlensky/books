9ebdf4f28ad8f0a0656dd1fbe79cae84bdd85bb0
Andrey Bogoyavlensky <abogoyavlensky@gmail.com>
2016-04-19 14:09:21 +0300

    Add example for os.path.splitext.

---
+++ python_essential_reference/page499.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+    path.to.file
+    ~~~~~~~~~~~~
+
+    Short description.
+
+    :copyright: (c) 2016 by Rambler&Co.
+"""
+
+import os
+
+if __name__ == '__main__':
+    # Получить имя файла и расширение отдельно в кортеже
+    print(os.path.splitext('foo.txt'))
