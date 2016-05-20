206475e663d21ab1fd250d6627770393ae596736
Andrey Bogoyavlensky <abogoyavlensky@gmail.com>
2016-05-20 16:50:45 +0300

    Simplify exericise 1.3.

--- sicp/part1/1.3.scm
+++ sicp/part1/1.3.scm
@@ -1,20 +1,16 @@
 (define (square x) (* x x))
 (define (sum x y) (+ (square x) (square y)))
-(define (=> x y) (or (> x y) (= x y)))
+(define (>= x y) (or (> x y) (= x y)))
 
 
   (define (max x y z)
-    (cond ((=> x y) (if (=> x z)
-                        (if (=> y z)
-                            (sum x y)
-                            (sum x z))
+    (cond ((>= x y) (if (and (> x z) (>= y z))
+                        (sum x y)
                         (sum x z)))
-          (else (if (=> y z)
-                    (if (=> x z)
-                        (sum y x) 
-                        (sum y z))
+          (else (if (and (>= y z) (>= x z))
+                    (sum y x)
                     (sum y z)))))
 
-(define (main) (max 5 6 9))
+(define (main) (max 4 6 3))
 
 (load "display.scm")
