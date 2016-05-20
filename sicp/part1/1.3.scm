e253a92380da335eb76b803921ef3f11ac3a8a9d
Andrey Bogoyavlensky <abogoyavlensky@gmail.com>
2016-05-20 15:01:05 +0300

    Rename >= to gte.

--- sicp/part1/1.3.scm
+++ sicp/part1/1.3.scm
@@ -1,11 +1,11 @@
 (define (square x) (* x x))
 (define (sum x y) (+ (square x) (square y)))
-(define (>= x y) (or (> x y) (= x y)))
+(define (gte x y) (or (> x y) (= x y)))
 
 
-(define (max x y z) (cond 
-                          ((>= x y) (if (>= x z) (if (>= y z) (sum x y)(sum x z)) (sum x z)))
-                          (else (if (>= y z) (if (>= x z) (sum y x) (sum y z)) (sum y z)))
+  (define (max x y z) (cond
+                          ((gte x y) (if (gte x z) (if (gte y z) (sum x y)(sum x z)) (sum x z)))
+                          (else (if (gte y z) (if (gte x z) (sum y x) (sum y z)) (sum y z)))
                           ))
 
 (define (main) (max 5 6 9))
