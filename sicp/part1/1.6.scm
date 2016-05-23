f94a777711f55d3f4850574ed1c9fdc738db6c34
Andrey Bogoyavlensky <abogoyavlensky@gmail.com>
2016-05-23 14:43:01 +0300

    1.6 exercise complete.

--- sicp/part1/1.6.scm
+++ sicp/part1/1.6.scm
@@ -1,3 +1,20 @@
 (load "common/sqrt.scm")
 
+(define (newif p t e)
+  (cond (p t)
+        (else e)))
+
+(define (sqrt_iter guess x)
+  (newif (good_enough? guess x 0.0001)
+      guess
+      (sqrt_iter (improve guess x) x)))
+
+(define (sqrt x)
+  (sqrt_iter 1.0 x))
+
 (print (sqrt 2))
+
+;Вычислить занчение с newif не получится, так как функции в scheme вычисляются
+;в аппликативном порядке. Это значит, что на момент запуска функции newif
+;должны быть известны все переданные значения, а это невозможно потому что
+;последним значением  e (else) передается рекурсивный запуск функции sqrt_iter.
