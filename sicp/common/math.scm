27c9ca57f80f71ea2c8f81782fc48cfb9f6dbe18
Andrey Bogoyavlensky <abogoyavlensky@gmail.com>
2016-05-24 18:07:45 +0300

    Add 1.13 example.

--- sicp/common/math.scm
+++ sicp/common/math.scm
@@ -5,3 +5,10 @@
 (define (>= x y) (or (> x y) (= x y)))
 
 (define (abs x) (if (> x 0) x (- x)))
+
+(define (pow x power)
+  (define (iter result x p)
+    (if (< p 1)
+        result
+        (iter (* result x) x (- p 1))))
+  (iter 1 x power))
--- sicp/part1/1.13.scm
+++ sicp/part1/1.13.scm
@@ -1,8 +1,30 @@
-(define (fib n)
+(load "common/math.scm")
+
+(define (fibonacci n)
   (define (iter a b count)
     (if (= count 0)
         b
         (iter (+ a b) a (- count 1))))
   (iter 1 0 n))
 
-(print (fib 11))
+
+(define (fi1)
+  (/ (+ 1 (sqrt 5)) 2))
+
+(define (fi2)
+  (/ (- 1 (sqrt 5))
+     2))
+
+
+(define (f_to_cmp1 p)
+  (/ (pow (fi1) p)
+     (sqrt 5)))
+
+(define (f_to_cmp2 p)
+  (/ (- (pow (fi1) p) (pow (fi2) p)) (sqrt 5)))
+
+
+(define n 10)
+(print (fibonacci n))
+(print (f_to_cmp1 n))
+(print (f_to_cmp2 n))
