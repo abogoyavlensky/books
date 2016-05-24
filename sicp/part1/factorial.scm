5a8ad47f3b2a0d7bd7de45d5fc48b9f163b22ab8
Andrey Bogoyavlensky <abogoyavlensky@gmail.com>
2016-05-24 09:59:32 +0300

    Add alternative example of factorial with positive counter.

--- sicp/part1/factorial.scm
+++ sicp/part1/factorial.scm
@@ -6,4 +6,15 @@
       1
       (* n (factorial (- n 1)))))
 
+
+(define (factorial2 n)
+  (define (fact_iter result count)
+    (if (> count n)
+        result
+        (fact_iter (* count result)
+                   (+ count 1))))
+
+  (fact_iter 1 1))
+
 (print (factorial 6))
+(print (factorial2 6))
