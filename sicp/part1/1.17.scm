fa91888fe96e116c231981274b2e0e6360dae81c
Andrey Bogoyavlensky <abogoyavlensky@gmail.com>
2016-05-26 12:50:12 +0300

    Add 1.17 example of multiple.

---
+++ sicp/part1/1.17.scm
@@ -0,0 +1,12 @@
+(load "common/math.scm")
+
+(define (double x) (+ x x))
+(define (halve x) (/ x 2))
+
+(define (mult a b)
+  (cond ((= b 0) 0)
+        ((even? b) (double (mult a (halve b))))
+        (else (+ a (mult a (- b 1))))))
+
+(print (* 34 738))
+(print (mult 34 738))
