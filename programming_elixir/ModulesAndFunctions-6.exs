e7fbf597d4ed9cf4bb80da9500579b083122bb4b
Bogoyavlensky Andrey <abogoyavlensky@gmail.com>
2016-10-02 22:47:46 +0300

    Add ModulesAndFunctions-6.exs

--- programming_elixir/ModulesAndFunctions-6.exs
+++ programming_elixir/ModulesAndFunctions-6.exs
@@ -1,5 +1,20 @@
 defmodule Chop do
-  def guess() do
+  def guess(actual, bottom..top) do
+    helper(actual, bottom..top, div(top, 2))
+  end
 
+  def helper(actual, bottom..top, current) when actual > current do
+    IO.puts("Is it #{current}")
+    helper(actual, current..top, current + div(top - current, 2))
+  end
+
+  def helper(actual, bottom..top, current) when actual < current do
+    IO.puts("Is it #{current}")
+    helper(actual, bottom..current, bottom + div(current - bottom, 2))
+  end
+
+  def helper(actual, bottom..top, current) when actual == current do
+    IO.puts("Is it #{current}")
+    IO.puts("#{current}")
   end
 end
