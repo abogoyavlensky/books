47713abdd05aefa5858ffc28e27dd7502ea4e51f
Bogoyavlensky Andrey <abogoyavlensky@gmail.com>
2016-10-12 20:21:36 +0300

    ELIXIR: Add custom take example.

--- programming_elixir/ListAndRecursion-5.exs
+++ programming_elixir/ListAndRecursion-5.exs
@@ -33,14 +33,28 @@ defmodule MyEnum do
        {[], list}
      end
    end
-
   defp _split(result = {_left , []}, _count), do: result
   defp _split(result = {_left , _right}, count) when count == 0, do: result
   defp _split({left, right}, count) when count != 0 do
-    [head | tail] = right
-    _split({left ++ [head], tail}, count - 1)
+    _split({left ++ [hd right], tl right}, count - 1)
   end
 
   defp _len([]), do: 0
   defp _len([_head | tail]), do: 1 + _len(tail)
+
+  def take(list, count) when count >= 0, do: _take(list, count, 0)
+  def take(list, count) when count < 0 do
+    len = _len(list)
+    skip = len + count
+    if skip >= 0 do
+      _take(list, -count, skip)
+    else
+      _take(list, len, 0)
+    end
+  end
+  defp _take([], _count, _skip), do: []
+  defp _take(_list, 0, _skip), do: []
+  defp _take([_head | tail], count, skip) when skip > 0, do: _take(tail, count, skip - 1)
+  defp _take([head | tail], count, skip), do: [head | _take(tail, count - 1, skip)]
+
 end
