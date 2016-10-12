8728ca9db903fe122b0bd751a8e706c22f8b65f1
Bogoyavlensky Andrey <abogoyavlensky@gmail.com>
2016-10-13 00:12:32 +0300

    ELIXIR: Update flatten.

--- programming_elixir/ListAndRecursion-6.exs
+++ programming_elixir/ListAndRecursion-6.exs
@@ -1,6 +1,5 @@
 defmodule MyModule do
   def flatten([]), do: []
-  def flatten(head) when not is_list(head), do: head
-  def flatten([head | tail]) when not is_list(head), do: [head] ++ flatten(tail)
   def flatten([head | tail]), do: flatten(head) ++ flatten(tail)
+  def flatten(head), do: [head]
 end
