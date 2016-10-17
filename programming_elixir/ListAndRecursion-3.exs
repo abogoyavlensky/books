47da3ede17f389205baf3e33e7d24faa869ff49d
Bogoyavlensky Andrey <abogoyavlensky@gmail.com>
2016-10-17 22:33:23 +0300

    ELIXIR: Improve comparison with z char in caesar example.

--- programming_elixir/ListAndRecursion-3.exs
+++ programming_elixir/ListAndRecursion-3.exs
@@ -1,9 +1,9 @@
 defmodule MyList do
   def caesar([], _value), do: []
-  def caesar([head | tail],  value) when (head + value) < 122 do
+  def caesar([head | tail],  value) when (head + value) < ?z do
     [head + value | caesar(tail, value)]
   end
-  def caesar([head | tail],  value) when (head + value) > 122 do
+  def caesar([head | tail],  value) when (head + value) > ?z do
     [head - value | caesar(tail, value)]
   end
 end
