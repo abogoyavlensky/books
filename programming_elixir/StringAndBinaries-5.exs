e3d3195ca579705379d40f3a5b2fc51ffca52251
Bogoyavlensky Andrey <abogoyavlensky@gmail.com>
2016-10-21 21:31:03 +0300

    ELIXIR: Add example for string with rjust.

---
+++ programming_elixir/StringAndBinaries-5.exs
@@ -0,0 +1,15 @@
+defmodule MyModule do
+  def center(list) do
+     len = Enum.max_by(list, &String.length/1) |> String.length
+    #  list |> Enum.each(_print(len))
+     list |> Enum.each(_print(len))
+  end
+
+  defp _print(len) do
+    fn word ->
+      word_len = String.length(word)
+      gaps = word_len + div(len - word_len, 2)
+      IO.puts String.rjust(word, gaps)
+    end
+  end
+end
