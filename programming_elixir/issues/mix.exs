121ccca876ab3aaf9209744a2fd6d07ae3ffb2d7
Bogoyavlensky Andrey <abogoyavlensky@gmail.com>
2016-10-29 13:31:16 +0300

    ELXIIR: Add ability to package app with escript.

--- programming_elixir/issues/mix.exs
+++ programming_elixir/issues/mix.exs
@@ -5,6 +5,7 @@ defmodule Issues.Mixfile do
     [app: :issues,
      version: "0.1.0",
      elixir: "~> 1.3",
+     escript: escript_config,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
@@ -14,7 +15,9 @@ defmodule Issues.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger, :httpoison]]
+    [
+      applications: [:logger, :httpoison, :jsx],
+    ]
   end
 
   # Dependencies can be Hex packages:
@@ -32,5 +35,9 @@ defmodule Issues.Mixfile do
       {:jsx, "~> 2.8"},
       {:credo, "~> 0.4.13", only: [:dev, :test]},
     ]
+  end
+  
+  defp escript_config do
+    [main_module: Issues.CLI]
   end
 end
