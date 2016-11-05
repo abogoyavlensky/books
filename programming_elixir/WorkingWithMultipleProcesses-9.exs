35e588f60981f1ecee7cfc67fc60649d32193029
Bogoyavlensky Andrey <abogoyavlensky@gmail.com>
2016-11-06 00:16:54 +0300

    ELIXIR: Add basic ability to count word in file in separate process.

--- programming_elixir/WorkingWithMultipleProcesses-9.exs
+++ programming_elixir/WorkingWithMultipleProcesses-9.exs
@@ -1,3 +1,33 @@
-defmodule  do
+defmodule Handler do
+  def handle(scheduler) do
+    send scheduler, {:ready, self}
+    receive do
+      {:handle, path, word, pid} -> 
+        send pid, {:answer, count_word(path, word), self}
+      {:shutdown} -> exit(:normal)
+    end
+  end
   
+  @doc """
+  Count number of word appearance in file by path.
+  """  
+  def count_word(path, word) do
+    path    
+    |> File.read!
+    |> String.split
+    |> Enum.count(&(&1 == word))
+  end
+end
+
+
+spawn(Handler, :handle, [self])
+
+receive do
+  {:ready, pid} -> send pid, {:handle, "cats/cats-001.txt", "cat", self}  
+end
+
+receive do
+  {:answer, count, pid} -> 
+    IO.puts inspect count
+    send pid, {:shutdown}
 end
