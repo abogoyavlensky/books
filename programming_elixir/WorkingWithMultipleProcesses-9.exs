d0879d764154c88f7d8d2504b8020cf9b1ea2061
Bogoyavlensky Andrey <abogoyavlensky@gmail.com>
2016-11-06 17:40:31 +0300

    ELIXIR: Complete example with counting word in hundred files by separate processes.

--- programming_elixir/WorkingWithMultipleProcesses-9.exs
+++ programming_elixir/WorkingWithMultipleProcesses-9.exs
@@ -2,8 +2,9 @@ defmodule Handler do
   def handle(scheduler) do
     send scheduler, {:ready, self}
     receive do
-      {:handle, path, word, pid} -> 
-        send pid, {:answer, count_word(path, word), self}
+      {:handle, args, pid} -> 
+        send pid, {:answer, args, count_word(args), self}
+        handle(pid)
       {:shutdown} -> exit(:normal)
     end
   end
@@ -11,8 +12,10 @@ defmodule Handler do
   @doc """
   Count number of word appearance in file by path.
   """  
-  def count_word(path, word) do
-    path    
+  def count_word([path: path, word: word]) do
+    # Add sleep() to illustraite advantages of concurrent programming
+    :timer.sleep(500)
+    path
     |> File.read!
     |> String.split
     |> Enum.count(&(&1 == word))
@@ -20,14 +23,30 @@ defmodule Handler do
 end
 
 
-spawn(Handler, :handle, [self])
-
-receive do
-  {:ready, pid} -> send pid, {:handle, "cats/cats-001.txt", "cat", self}  
-end
-
-receive do
-  {:answer, count, pid} -> 
-    IO.puts inspect count
-    send pid, {:shutdown}
+defmodule Scheduler do
+  def run(module, dir, word) do
+    files = dir |> File.ls! |> Enum.map(&("#{dir}/#{&1}"))
+    files
+    |> Enum.map(fn _ -> spawn(module, :handle, [self]) end)
+    |> schedule_processes(files, word, [])
+  end
+  
+  def schedule_processes(processes, queue, word, result) do
+    receive do
+      {:ready, pid} when length(queue) > 0 ->
+        [next | tail] = queue
+        send pid, {:handle, [path: next, word: word], self}
+        schedule_processes(processes, tail, word, result)
+      
+      {:ready, pid} when length(processes) > 1 ->
+        send pid, {:shutdown}
+        schedule_processes(List.delete(processes, pid), queue, word, result)
+      {:ready, pid} -> 
+        send pid, {:shutdown}
+        result
+              
+      {:answer, [path: path, word: word], count, _pid} -> 
+        schedule_processes(processes, queue, word, [{path, count} | result])
+    end
+  end
 end
