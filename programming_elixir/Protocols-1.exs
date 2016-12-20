0be391430e296177c77eaa8701d9712bd276a090
Bogoyavlensky Andrey <abogoyavlensky@gmail.com>
2016-12-20 22:22:41 +0300

    ELIXIR: Add rot13 function to Caesar protocol.

--- programming_elixir/Protocols-1.exs
+++ programming_elixir/Protocols-1.exs
@@ -1,11 +1,13 @@
 defprotocol Caesar do
   @fallback_to_any true
   def encrypt(string, shift \\ 0)
+  def rot13(string)
 end
 
 defimpl Caesar, for: [BitString, List] do
   @first ?a - 1
   @last ?z
+  @rot13_shift 13
 
   def encrypt(string, shift) when is_list(string) do
     string
@@ -16,6 +18,10 @@ defimpl Caesar, for: [BitString, List] do
     |> to_charlist
     |> Enum.map(&_shift(&1, shift))
     |> to_string
+  end
+
+  def rot13(string) do
+    encrypt(string, @rot13_shift)
   end
 
   def _shift(char, shift) do
@@ -31,8 +37,12 @@ end
 
 defimpl Caesar, for: Any do
   def encrypt(_string, _shift), do: "Argument must be String or List"
+  def rot13(_string), do: "Argument must be String or List"
 end
 
 
 IO.inspect Caesar.encrypt('abcxz', 1)
 IO.inspect Caesar.encrypt("abcxz", 1)
+
+IO.inspect Caesar.rot13('abcxz')
+IO.inspect Caesar.rot13("abcxz")
