a233e5f0d7a8f625767b979792f149b3f0ca7712
Bogoyavlensky Andrey <abogoyavlensky@gmail.com>
2017-05-16 22:58:37 +0300

    SICP: Solve 1.22 task about searching primes.

--- sicp/README.md
+++ sicp/README.md
@@ -1,7 +1,21 @@
 To perform script you should install scm and run any script:
 ------------------------------------------------------------
 
+## Install Scheme:
+
 ```shell
 $ sudo apt-get install scm
 $ scm -f part1/1.3.scm
 ```
+
+
+## Install Racket:
+
+```shell
+$ sudo apt-get install racket
+$ racket -f part1/1.22.scm
+```
+
+*Some of examples work correctly just with `racket`.
+If you see `#lang racket` on the first line of the file
+you should start it with `racket`.*
--- sicp/part1/1.22.scm
+++ sicp/part1/1.22.scm
@@ -1,29 +1,74 @@
-(load "part1/1.21.scm")
+; Поиск наименьшего делителя
+(define (square x) (* x x))
+(define (divides? a b)
+  (= (remainder b a) 0))
 
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+; Проверка на простоту
 (define (prime? n)
   (= n (smallest-divisor n)))
 
-(define (report-prime elapsed-time)
-  (display "***")
+(define (runtime) (current-inexact-milliseconds))
+
+; Тест на простоту числа с измерением времени
+(define (timed-prime-test n)
+  (start-prime-test n (runtime)))
+
+(define (start-prime-test n start-time)
+  (if (prime? n)
+      (report-prime n (- (runtime) start-time))
+      #f))
+
+(define (report-prime n elapsed-time)
+  (print n)
+  (print "***")
+  (print elapsed-time)
   (newline)
-  (display elapsed-time)
+  #t)
+
+(define (search-for-primes begin end limit)
+  (define (find begin counter)
+    (define current-prime? (timed-prime-test begin))
+    (cond ((or (= begin end) (= counter limit)) (print "Finish"))
+          ((even? end) (find (+ begin 1) (get_counter current-prime? counter)))
+          (else (find (+ begin 2) (get_counter current-prime? counter)))))
+  (define (get_counter check c)
+    (if check (+ c 1) c))
+  (find begin 0)
   (newline))
 
-(define (timed-prime-test n)
-  (define start-time (current-time))
-  (newline)
-  (display n)
-  (newline)
-  (if (prime? n)
-      (report-prime (- (current-time) start-time))))
 
+(search-for-primes 1 1000 3)
+(search-for-primes 1000 10000 3)
+(search-for-primes 10000 100000 3)
+(search-for-primes 100000 1000000 3)
+(search-for-primes 1000000 10000000 3)
 
-(timed-prime-test 7)
-
-
-(define (check-prime-in-range bottom top)
-  (cond ((= bottom top) (newline))
-        ((divides? bottom 2) (timed-prime-test bottom))
-        (else (check-prime-in-range (+ bottom 1) top))))
-
-(check-prime-in-range 100 1000)
+; Вывод:
+;1"***"0.05908203125
+;2"***"0.0009765625
+;3"***"0.0
+;"Finish"
+;1009"***"0.001953125
+;1013"***"0.0009765625
+;1019"***"0.0009765625
+;"Finish"
+;10007"***"0.004150390625
+;10009"***"0.00390625
+;10037"***"0.00390625
+;"Finish"
+;100003"***"0.012939453125
+;100019"***"0.012939453125
+;100043"***"0.012939453125
+;"Finish"
+;1000003"***"0.041015625
+;1000033"***"0.041015625
+;1000037"***"0.041015625
+;"Finish"
