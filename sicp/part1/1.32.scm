c11e8811b4fe74ea9948c28357f5d6e0666bfc6a
Bogoyavlensky Andrey <abogoyavlensky@gmail.com>
2017-05-30 23:37:06 +0300

    SICP: Finish with accumulate function.

--- sicp/part1/1.32.scm
+++ sicp/part1/1.32.scm
@@ -5,17 +5,22 @@
       (combiner (term a)
                 (accumulate combiner null-value term (next a) next b))))
 
+; Итеративный процесс
+(define (accumulate-i combiner null-value term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (combiner (term a) result))))
+  (iter a null-value))
+
+
 (define (id x) x)
 (define (inc x) (+ x 1))
 
-(define (sum term a next b)
-  (accumulate + 0 term a next b))
+; Sum
+(print (accumulate + 0 id 0 inc 10))  ; =55
+(print (accumulate-i + 0 id 0 inc 10))  ; =55
 
-(define (product term a next b)
-  (accumulate * 1 term a next b))
-
-(define (factorial n)
-  (product id 1 inc n))
-
-(print (factorial 5))  ; =120
-(print (sum id 0 inc 10))  ; =55
+; Product)
+(print (accumulate * 1 id 1 inc 5))  ; =120
+(print (accumulate-i * 1 id 1 inc 5))  ; =120
