2f3017ae28cb387234c69e9978b27a66ae8e8193
Bogoyavlensky Andrey <abogoyavlensky@gmail.com>
2017-06-01 23:52:12 +0300

    SICP: Add ability to count fi with fixed-point

---
+++ sicp/part1/1.35.scm
@@ -0,0 +1,17 @@
+(define (fixed-point f first-guess)
+  (define tolerance 0.00001)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try (f next)))))
+  (try first-guess))
+
+(define fi
+  (fixed-point (lambda (x) (+ 1 (/ 1 x)))
+               1.0))
+
+(print (fixed-point cos 1.0))
+(print fi)
