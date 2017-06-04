d6924a30e80e2f77ebf4573a348e6da56cc6240a
Bogoyavlensky Andrey <abogoyavlensky@gmail.com>
2017-06-04 14:14:38 +0300

    SICP: Add ability to resolve cubic equals

---
+++ sicp/part1/1.40.scm
@@ -0,0 +1,34 @@
+(define (fixed-point f first-guess)
+  (define tolerance 0.00001)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try (f next)))))
+  (try first-guess))
+
+(define (newton-transform g)
+  (lambda (x)
+    (- x (/ (g x) ((deriv g) x)))))
+
+(define (deriv g)
+  (lambda (x)
+    (/ (- (g (+ x dx)) (g x))
+       dx)))
+
+(define dx 0.00001)
+
+(define (square x) (* x x))
+(define (cube x) (* x x x))
+
+(define (cubic a b c)
+  (lambda (x)
+    (+ (cube x) (* a (square x)) (* b x) c)))
+
+(define (newtons-method g guess)
+  (fixed-point (newton-transform g) guess))
+
+
+(print (newtons-method (cubic -3 3 -1) 1))  ; =1.0
