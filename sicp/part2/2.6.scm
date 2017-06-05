e450a4b952f1ee5774b66124a098cf3982443ae0
Bogoyavlensky Andrey <abogoyavlensky@gmail.com>
2017-06-05 22:47:09 +0300

    SICP-2.6: Fix Church numerals

--- sicp/part2/2.6.scm
+++ sicp/part2/2.6.scm
@@ -4,29 +4,29 @@
 
 ; Подстановка one
 (define one (add-1 zero))
-(add-1 zero)
-(add-1 (lambda (f) (lambda (x) x)))
-(lambda (f) (lambda (x) (f (((lambda (f) (lambda (x) x)) f) x))))
-(lambda (f) (lambda (x) (f x)))  ; =one
+;(add-1 zero)
+;(add-1 (lambda (f) (lambda (x) x)))
+;(lambda (f) (lambda (x) (f (((lambda (f) (lambda (x) x)) f) x))))
+;(lambda (f) (lambda (x) (f x)))  ; =one
 
 
 ; Подстановка two
 (define two (add-1 one))
-(add-1 (lambda (f) (lambda (x) (f x))))
-(lambda (f) (lambda (x) (f (((lambda (f) (lambda (x) (f x))) f) x))))
-(lambda (f) (lambda (x) (f ((lambda (x) (f x)) x))))
-(lambda (f) (lambda (x) (f (f x))))  ; =two
+;(add-1 (lambda (f) (lambda (x) (f x))))
+;(lambda (f) (lambda (x) (f (((lambda (f) (lambda (x) (f x))) f) x))))
+;(lambda (f) (lambda (x) (f ((lambda (x) (f x)) x))))
+;(lambda (f) (lambda (x) (f (f x))))  ; =two
+
 
 ; Определение сложения
 (define (add a b)
-  (lambda (f) (lambda (x) (f ((b (a f)) x)))))
+  (lambda (f) (lambda (x) ((a f) ((b f) x)))))
 
-
-; Подстановка
-(add one two)
-(add (lambda (f) (lambda (x) (f x)))
-     (lambda (f) (lambda (x) (f (f x)))))
-(lambda (f) (lambda (x) (f (((lambda (f) (lambda (x) (f (f x)))) ((lambda (f) (lambda (x) (f x))) f)) x))))
-(lambda (f) (lambda (x) (f (((lambda (f) (lambda (x) (f (f x)))) (lambda (x) (f x))) x))))
-(lambda (f) (lambda (x) (f ((lambda (x) ((lambda (x) (f x)) ((lambda (x) (f x)) x))) x))))
-(lambda (f) (lambda (x) (f (f (f x)))))  ; one + two = three
+; Проверка
+(define (inc n) (+ n 1))
+(define three (add one two))
+(define four (add two two))
+(define seven (add three four))
+(print ((three inc) 0))
+(print ((four inc) 0))
+(print ((seven inc) 0))
