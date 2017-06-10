cd76da266cbfe31411088540daea8b2180a06be0
Bogoyavlensky Andrey <abogoyavlensky@gmail.com>
2017-06-10 20:49:39 +0300

    SICP-2.27: Add implementation of iterative deep-reverse

--- sicp/part2/2.18.scm
+++ sicp/part2/2.18.scm
@@ -1,3 +1,4 @@
+; Итеративный процесс
 (define (reverse l)
   (define (iter tail result)
     (if (null? tail)
@@ -5,4 +6,14 @@
         (iter (cdr tail) (cons (car tail) result))))
   (iter l '()))
 
+
+; Рекурсивный процесс
+(define (reverse-rec items)
+  (if (null? items)
+      '()
+      (append (reverse-rec (cdr items))
+              (list (car items)))))
+
+
 (print (reverse (list 1 4 9 16 25)))
+(print (reverse-rec (list 1 4 9 16 25)))
--- sicp/part2/2.27.scm
+++ sicp/part2/2.27.scm
@@ -1,44 +1,15 @@
-(define (reverse-i items)
+(define (deep-reverse items)
   (define (iter tail result)
-    (if (null? tail)
-        result
-        (iter (cdr tail) (cons (car tail) result))))
+    (cond ((null? tail) result)
+          ((pair? (car tail))
+           (iter (cdr tail)
+                 (cons (iter (car tail) '()) result)))
+          (else (iter (cdr tail) (cons (car tail) result)))))
   (iter items '()))
 
-(define (reverse items)
-  (if (null? items)
-      '()
-      (append (reverse (cdr items))
-              (list (car items)))))
 
-;(define (deep-reverse items)
-;  (cond
-;        ((not (pair? items)) items)
-;        ((= (length items) 1) (deep-reverse (car items)))
-;        (else (list (deep-reverse (cdr items))
-;                    (deep-reverse (car items))))))
+(define x (list (list 1 2) (list 3 4)))
+(define y (list (list 1 2 3 (list 5 6)) (list 3 4 5)))
 
-(define (deep-reverse items)
-  (cond
-        ((not (pair? items)) items)
-        ((= (length items) 1) (deep-reverse (car items)))
-        (else (append (let ((a (deep-reverse (cdr items))))
-                        (if (list? a)
-                            a
-                            (list a)))
-                      (let ((b (deep-reverse (car items))))
-                        (if (list? b)
-                            b
-                            (list b)))))))
-
-
-(define x (list 1 2 3 4 5))
-(define y (list (list 1 2) (list 3 4)))
-(define z (list (list 1 2 3 (list 5 6)) (list 3 4 5)))
-
-
-(print x)  ; =((1 2) (3 4))
-(print (reverse x))
 (print (deep-reverse x))
 (print (deep-reverse y))
-(print (deep-reverse z))
