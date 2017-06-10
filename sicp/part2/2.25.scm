04ceafff3df2a30ccef23d9daad4f5a37cff6110
Bogoyavlensky Andrey <abogoyavlensky@gmail.com>
2017-06-10 15:29:30 +0300

    SICP-2.25: Add ability to extract 7

---
+++ sicp/part2/2.25.scm
@@ -0,0 +1,10 @@
+(define l1 (list 1 3 (list 5 7) 9))
+(define l2 (list (list 7)))
+(define l3 (list 1 (list 2 (list 3 (list 4 (list 5 (list 6 7)))))))
+
+(print l1)
+(print (car (cdr (car (cdr (cdr l1))))))
+(print l2)
+(print (car (car l2)))
+(print l3)
+(print (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr l3)))))))))))))
