b020413907aefa9af1dcf3283e04048a504b7ac1
Bogoyavlensky Andrey <abogoyavlensky@gmail.com>
2017-06-10 20:54:28 +0300

    SICP-2.27: Add recursive implementation of deep-reverse

--- sicp/part2/2.27.scm
+++ sicp/part2/2.27.scm
@@ -1,3 +1,4 @@
+; Итеративный процесс
 (define (deep-reverse items)
   (define (iter tail result)
     (cond ((null? tail) result)
@@ -7,9 +8,23 @@
           (else (iter (cdr tail) (cons (car tail) result)))))
   (iter items '()))
 
+; Рекурсивный процесс
+(define (deep-reverse-rec items)
+  (if (null? items)
+      '()
+      (append (deep-reverse-rec (cdr items))
+              (list
+                (if (pair? (car items))
+                    (deep-reverse-rec (car items))
+                    (car items))))))
+
+
+
 
 (define x (list (list 1 2) (list 3 4)))
 (define y (list (list 1 2 3 (list 5 6)) (list 3 4 5)))
 
 (print (deep-reverse x))
+(print (deep-reverse-rec x))
 (print (deep-reverse y))
+(print (deep-reverse-rec y))
