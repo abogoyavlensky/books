7a7d2890ff9f57d7deb1e3aba618535dddef30e8
Bogoyavlensky Andrey <abogoyavlensky@gmail.com>
2017-06-12 01:02:52 +0300

    SICP-2.36: Try to solve accumulate-n (not finished)

--- sicp/part2/2.36.scm
+++ sicp/part2/2.36.scm
@@ -1,3 +1,9 @@
+(define (map proc items)
+  (if (null? items)
+      '()
+      (cons (proc (car items))
+            (map proc (cdr items)))))
+
 (define (accumulate op initial sequence)
   (if (null? sequence)
       initial
@@ -7,8 +13,17 @@
 (define (accumulate-n op init seqs)
   (if (null? (car seqs))
       '()
-      (cons (accumulate op init ?)
-            (accumulate-n op init ?))))
+      (cons (accumulate op
+                        init
+                        (let ((rest (car seqs)))
+                          (append rest
+                                  (map (lambda (x) (cons (car seqs) x))
+                                       rest))))
+            (accumulate-n op
+                          init
+                          (if (null? (cdr seqs))
+                              (list '())
+                              (cdr seqs))))))
 
 
 (define s (list (list 1 2 3) (list 4 5 6) (list 7 8 9) (list 10 11 12)))
