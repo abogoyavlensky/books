4f27d76d9797cfabce8f7c08c121870636b872df
Bogoyavlensky Andrey <abogoyavlensky@gmail.com>
2017-06-13 07:24:06 +0300

    SICP-2.36: Add implementation of accumulate-n

--- sicp/part2/2.36.scm
+++ sicp/part2/2.36.scm
@@ -13,17 +13,8 @@
 (define (accumulate-n op init seqs)
   (if (null? (car seqs))
       '()
-      (cons (accumulate op
-                        init
-                        (let ((rest (car seqs)))
-                          (append rest
-                                  (map (lambda (x) (cons (car seqs) x))
-                                       rest))))
-            (accumulate-n op
-                          init
-                          (if (null? (cdr seqs))
-                              (list '())
-                              (cdr seqs))))))
+      (cons (accumulate op init (map (lambda (x) (car x)) seqs))
+            (accumulate-n op init (map (lambda (x) (cdr x)) seqs)))))
 
 
 (define s (list (list 1 2 3) (list 4 5 6) (list 7 8 9) (list 10 11 12)))
