7608ebc280283e4edf01112e5632d60c26ce8443
Bogoyavlensky Andrey <abogoyavlensky@gmail.com>
2017-06-15 20:28:58 +0300

    SICP-2.37: Add transpose implementation

--- sicp/part2/2.37.scm
+++ sicp/part2/2.37.scm
@@ -17,10 +17,12 @@
   (map (lambda (x) (dot-product x v)) m))
 
 (define (transpose mat)
-  (accumulate-n () () mat))
+  (accumulate-n cons '() mat))
+
 
 (define v (list 1 2 3 4))
 (define m (list (list 1 2 3 4) (list 4 5 6 6) (list 6 7 8 9)))
 
 (print (dot-product v v))  ; =30
 (print (matrix-*-vector m v))  ; =(30 56 80)
+(print (transpose m))  ; =((1 4 6) (2 5 7) (3 6 8) (4 6 9))
