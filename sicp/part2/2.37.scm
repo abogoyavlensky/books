29cec3e2e008ca58b56b05d0f40230047055a803
Bogoyavlensky Andrey <abogoyavlensky@gmail.com>
2017-06-15 20:19:10 +0300

    SICP-2.37: Add ability multiplication matrix on vector

--- sicp/part2/2.37.scm
+++ sicp/part2/2.37.scm
@@ -14,10 +14,13 @@
   (accumulate + 0 (map * v w)))
 
 (define (matrix-*-vector m v)
-  (map  m))
+  (map (lambda (x) (dot-product x v)) m))
 
+(define (transpose mat)
+  (accumulate-n () () mat))
 
 (define v (list 1 2 3 4))
 (define m (list (list 1 2 3 4) (list 4 5 6 6) (list 6 7 8 9)))
 
 (print (dot-product v v))  ; =30
+(print (matrix-*-vector m v))  ; =(30 56 80)
