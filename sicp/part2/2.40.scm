a1ffa80c4902a2249db77101453bb34859b432e5
Bogoyavlensky Andrey <abogoyavlensky@gmail.com>
2017-06-18 23:47:09 +0300

    SICP-2.40: Started to make unique-pairs

---
+++ sicp/part2/2.40.scm
@@ -0,0 +1,13 @@
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (enumerate-interval a b)
+  (if (> a b)
+      '()
+      (cons a (enumerate-interval (+ a 1) b))))
+
+
+(print (enumerate-interval 1 6))
