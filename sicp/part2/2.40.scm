6cf8963dc549b70be0ceb3e0799f5f5924d58f43
Bogoyavlensky Andrey <abogoyavlensky@gmail.com>
2018-01-17 01:11:09 +0300

    SICP-2.40: Complete searching prime sum pairs

--- sicp/part2/2.40.scm
+++ sicp/part2/2.40.scm
@@ -1,3 +1,20 @@
+(define (square x) (* x x))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+; Проверка на простоту из файла 1.22.scm
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
 (define (accumulate op initial sequence)
   (if (null? sequence)
     initial
@@ -19,10 +36,31 @@
   (flatmap
     (lambda (i)
       (map (lambda (j) (list i j))
-           (enumerate-interval 1 (- n 1))))
+           (enumerate-interval 1 (- i 1))))
     (enumerate-interval 1 n)))
+
+(define (filter pred sequence)
+  (cond ((null? sequence) '())
+    ((pred (car sequence))
+     (cons (car sequence)
+           (filter pred (cdr sequence))))
+    (else (filter pred (cdr sequence)))))
+
+(define (prime-sum? pair)
+  (prime? (+ (car pair) (cadr pair))))
+
+(define (make-pair-sum pair)
+  (list (car pair) (cadr pair) (+ (car pair) (cadr pair))))
+
+(define (prime-sum-pairs n)
+  (map make-pair-sum
+       (filter prime-sum?
+               (unique-pairs n))))
 
 (print (unique-pairs 6))
 ;((1 1) (1 2) (1 3) (1 4) (1 5) (2 1) (2 2) (2 3) (2 4) (2 5) (3 1) (3 2)
 ;(3 3) (3 4) (3 5) (4 1) (4 2) (4 3) (4 4) (4 5) (5 1) (5 2) (5 3) (5 4)
 ; (5 5) (6 1) (6 2) (6 3) (6 4) (6 5)
+
+(print (prime-sum-pairs 6))
+; ((2 1 3) (3 2 5) (4 1 5) (4 3 7) (5 2 7) (6 1 7) (6 5 11))
